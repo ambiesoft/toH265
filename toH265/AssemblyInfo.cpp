3f519c9396acfe4ad86aa7daf4c404ac521bf1e5
ambiesoft <ambiesoft.trueff@gmail.com>
2020-05-08 14:19:23 +0900

    dist

--- toH265/AssemblyInfo.cpp
+++ toH265/AssemblyInfo.cpp
@@ -20,7 +20,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-[assembly:AssemblyVersionAttribute("1.0.41.*")];
+[assembly:AssemblyVersionAttribute("1.0.42.*")];
 
 [assembly:ComVisible(false)];
 
