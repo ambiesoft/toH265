9b9f4e0d92cbf92a4c6650c2fc362ad3fd77931a
ambiesoft <ambiesoft.trueff@gmail.com>
2021-01-03 04:24:17 +0900

    some

--- toH265/AssemblyInfo.cpp
+++ toH265/AssemblyInfo.cpp
@@ -20,7 +20,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-[assembly:AssemblyVersionAttribute("1.0.55.*")];
+[assembly:AssemblyVersionAttribute("1.0.56.*")];
 
 [assembly:ComVisible(false)];
 
