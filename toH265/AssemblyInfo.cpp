f69135f42f4b96772940ec3b2c514a232023008d
ambiesoft <ambiesoft.trueff@gmail.com>
2021-03-15 21:41:27 +0900

    Fix: Control properties

--- toH265/AssemblyInfo.cpp
+++ toH265/AssemblyInfo.cpp
@@ -20,7 +20,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-[assembly:AssemblyVersionAttribute("1.0.77.*")];
+[assembly:AssemblyVersionAttribute("1.0.78.*")];
 
 [assembly:ComVisible(false)];
 
