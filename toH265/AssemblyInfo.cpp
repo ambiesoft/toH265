d5933a81cb42fcfecf6d7f13127706ce09d8ac55
ambiesoft <ambiesoft.trueff@gmail.com>
2025-10-26 22:07:50 +0900

    Updated library AfterRun Upgrade .NET to 4.8.1

--- toH265/AssemblyInfo.cpp
+++ toH265/AssemblyInfo.cpp
@@ -20,7 +20,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
-[assembly:AssemblyVersionAttribute("1.2.5.*")];
+[assembly:AssemblyVersionAttribute("1.3.1.*")];
 
 [assembly:ComVisible(false)];
 
