18dbefaf658aedca7ece09c736e49d6da8f8d3dc
ambiesoft <ambiesoft.trueff@gmail.com>
2025-11-19 12:37:18 +0900

    dist

--- dist.bat
+++ dist.bat
@@ -1,3 +1,3 @@
-C:\local\python3\python.exe "%~dp0..\distSolution\distSolution.py" "%~dp0dist.json"
+C:\local\python3\python.exe "%~dp0..\distSolution\distSolution.py" "%~dp0dist.json" --skip-upload --skip-hashcheck
 
 pause
